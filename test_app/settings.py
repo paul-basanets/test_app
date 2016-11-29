5a2ed50e97df4455773f55f915f4c765d2d9db29
Paul <paulbasanets@gmail.com>
2016-11-29 11:14:07 +0200

    Bootstrap + templates

--- test_app/settings.py
+++ test_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0jfmkre3nzx4mjt+zunv-hgvk_*s6bnk!yxlbhm1p9qb1ye&*('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
